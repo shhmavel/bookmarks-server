6f16c8044da0dc2b73f9d0ea2635d6566c9a05b4
Christina Labay <Christina.m.labay@gmail.com>
2019-04-27 22:36:26 -0500

    initial commit

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+    id: 1,
+    title: 'Google bookmark',
+    url: 'www.google.com',
+    description: 'this is the google search',
+    rating: 3.5
+}]
+
+module.exports = {bookmarks}
